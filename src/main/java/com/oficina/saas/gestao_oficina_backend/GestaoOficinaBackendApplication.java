7dbf31a02b33c8d31638bfeab58c95e3605737d3
Felipe Garcia <felipegarc.tech@gmail.com>
2025-08-04 20:58:00 -0300

    feat: initial commit with Spring Boot boilerplate

---
+++ src/main/java/com/oficina/saas/gestao_oficina_backend/GestaoOficinaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.oficina.saas.gestao_oficina_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoOficinaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoOficinaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oficina/saas/gestao_oficina_backend/GestaoOficinaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oficina.saas.gestao_oficina_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoOficinaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
